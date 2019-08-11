54b87283ed15cff58c1d23cfe0958130ee1f6c9b
Ogarkov Mikhail <m.a.ogarkov@gmail.com>
2019-08-11 16:18:27 +0200

    wrapper works only in reading mode (#10)

--- src/BypassFinals.php
+++ src/BypassFinals.php
@@ -123,7 +123,7 @@ class BypassFinals
 	public function stream_open($path, $mode, $options, &$openedPath)
 	{
 		$usePath = (bool) ($options & STREAM_USE_PATH);
-		if (pathinfo($path, PATHINFO_EXTENSION) === 'php') {
+		if ($mode === 'rb' && pathinfo($path, PATHINFO_EXTENSION) === 'php') {
 			$content = $this->native('file_get_contents', $path, $usePath, $this->context);
 			if ($content === false) {
 				return false;
