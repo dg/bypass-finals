8b3218acb394952780ca61be3812f476f17d63db
Pierre Hennequart <pierre@janalis.com>
2020-05-12 18:44:23 +0200

    test touch

---
+++ tests/BypassFinals/BypassFinals.touch.phpt.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use Tester\Assert;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Tester\Environment::setup();
+
+DG\BypassFinals::enable();
+
+Assert::noError(function () {
+    touch('known');
+});
+
+Assert::noError(function () {
+    touch('known', time());
+});
+
+Assert::error(function () {
+    touch('known', null);
+}, TypeError::class);
+
+Assert::noError(function () {
+    unlink('known');
+});
