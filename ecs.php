575e73a51eacbd4fbe689ed80315fb3354ac5122
David Grudl <david@grudl.com>
2020-10-25 17:24:59 +0100

    updated nette/coding-standard

---
+++ ecs.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Rules for Nette Coding Standard
+ * https://github.com/nette/coding-standard
+ */
+
+declare(strict_types=1);
+
+
+return function (Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator): void {
+	$containerConfigurator->import(PRESET_DIR . '/php71.php');
+
+	$parameters = $containerConfigurator->parameters();
+
+	$parameters->set('skip', [
+		PHP_CodeSniffer\Standards\PSR1\Sniffs\Methods\CamelCapsMethodNameSniff::class => [
+			'src/BypassFinals.php',
+		],
+	]);
+};
