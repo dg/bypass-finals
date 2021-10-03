66079e148dfe892c929832e46a20e92da2d7ad8f
David <david@grudl.com>
2021-10-03 17:22:16 +0200

    divided into classes BypassFinals & NativeWrapper

--- src/BypassFinals.php
+++ src/BypassFinals.php
@@ -15,8 +15,8 @@ class BypassFinals
 	/** @var resource|null */
 	public $context;
 
-	/** @var resource|null */
-	private $handle;
+	/** @var object|null */
+	private $wrapper;
 
 	/** @var array */
 	private static $pathWhitelist = ['*'];
@@ -24,6 +24,7 @@ class BypassFinals
 
 	public static function enable(): void
 	{
+		NativeWrapper::$outerWrapper = self::class;
 		stream_wrapper_unregister(self::PROTOCOL);
 		stream_wrapper_register(self::PROTOCOL, self::class);
 	}
@@ -39,204 +40,39 @@ class BypassFinals
 	}
 
 
-	public function dir_closedir(): void
+	public function stream_open(string $path, string $mode, int $options, ?string &$openedPath): bool
 	{
-		closedir($this->handle);
+		$this->wrapper = $this->createUnderlyingWrapper();
+		if (!$this->wrapper->stream_open($path, $mode, $options, $openedPath)) {
+			return false;
+		}
+
+		if ($mode === 'rb' && pathinfo($path, PATHINFO_EXTENSION) === 'php' && self::isPathInWhiteList($path)) {
+			$content = '';
+			while (!$this->wrapper->stream_eof()) {
+				$content .= $this->wrapper->stream_read(8192);
+			}
+
+			$modified = self::removeFinals($content);
+			if ($modified === $content) {
+				$this->wrapper->stream_seek(0);
+			} else {
+				$this->wrapper->stream_close();
+				$this->wrapper = new NativeWrapper;
+				$this->wrapper->handle = tmpfile();
+				$this->wrapper->stream_write($modified);
+				$this->wrapper->stream_seek(0);
+			}
+		}
+
+		return true;
 	}
 
 
 	public function dir_opendir(string $path, int $options): bool
 	{
-		$this->handle = $this->context
-			? $this->native('opendir', $path, $this->context)
-			: $this->native('opendir', $path);
-		return (bool) $this->handle;
-	}
-
-
-	public function dir_readdir()
-	{
-		return readdir($this->handle);
-	}
-
-
-	public function dir_rewinddir(): bool
-	{
-		rewinddir($this->handle);
-		return true;
-	}
-
-
-	public function mkdir(string $path, int $mode, int $options): bool
-	{
-		$recursive = (bool) ($options & STREAM_MKDIR_RECURSIVE);
-		return $this->context
-			? $this->native('mkdir', $path, $mode, $recursive, $this->context)
-			: $this->native('mkdir', $path, $mode, $recursive);
-	}
-
-
-	public function rename(string $pathFrom, string $pathTo): bool
-	{
-		return $this->context
-			? $this->native('rename', $pathFrom, $pathTo, $this->context)
-			: $this->native('rename', $pathFrom, $pathTo);
-	}
-
-
-	public function rmdir(string $path, int $options): bool
-	{
-		return $this->context
-			? $this->native('rmdir', $path, $this->context)
-			: $this->native('rmdir', $path);
-	}
-
-
-	public function stream_cast(int $castAs)
-	{
-		return $this->handle;
-	}
-
-
-	public function stream_close(): void
-	{
-		fclose($this->handle);
-	}
-
-
-	public function stream_eof(): bool
-	{
-		return feof($this->handle);
-	}
-
-
-	public function stream_flush(): bool
-	{
-		return fflush($this->handle);
-	}
-
-
-	public function stream_lock(int $operation): bool
-	{
-		return $operation
-			? flock($this->handle, $operation)
-			: true;
-	}
-
-
-	public function stream_metadata(string $path, int $option, $value): bool
-	{
-		switch ($option) {
-			case STREAM_META_TOUCH:
-				return $this->native('touch', $path, $value[0] ?? time(), $value[1] ?? time());
-			case STREAM_META_OWNER_NAME:
-			case STREAM_META_OWNER:
-				return $this->native('chown', $path, $value);
-			case STREAM_META_GROUP_NAME:
-			case STREAM_META_GROUP:
-				return $this->native('chgrp', $path, $value);
-			case STREAM_META_ACCESS:
-				return $this->native('chmod', $path, $value);
-		}
-
-		return false;
-	}
-
-
-	public function stream_open(string $path, string $mode, int $options, ?string &$openedPath): bool
-	{
-		$usePath = (bool) ($options & STREAM_USE_PATH);
-		if ($mode === 'rb' && pathinfo($path, PATHINFO_EXTENSION) === 'php' && self::isPathInWhiteList($path)) {
-			$content = $this->native('file_get_contents', $path, $usePath, $this->context);
-			if ($content === false) {
-				return false;
-			}
-			$modified = self::removeFinals($content);
-			if ($modified !== $content) {
-				$this->handle = tmpfile();
-				$this->native('fwrite', $this->handle, $modified);
-				$this->native('fseek', $this->handle, 0);
-				return true;
-			}
-		}
-		$this->handle = $this->context
-			? $this->native('fopen', $path, $mode, $usePath, $this->context)
-			: $this->native('fopen', $path, $mode, $usePath);
-		return (bool) $this->handle;
-	}
-
-
-	public function stream_read(int $count)
-	{
-		return fread($this->handle, $count);
-	}
-
-
-	public function stream_seek(int $offset, int $whence = SEEK_SET): bool
-	{
-		return fseek($this->handle, $offset, $whence) === 0;
-	}
-
-
-	public function stream_set_option(int $option, int $arg1, ?int $arg2): bool
-	{
-		return false;
-	}
-
-
-	public function stream_stat()
-	{
-		return fstat($this->handle);
-	}
-
-
-	public function stream_tell()
-	{
-		return ftell($this->handle);
-	}
-
-
-	public function stream_truncate(int $newSize): bool
-	{
-		return ftruncate($this->handle, $newSize);
-	}
-
-
-	public function stream_write(string $data)
-	{
-		return fwrite($this->handle, $data);
-	}
-
-
-	public function unlink(string $path): bool
-	{
-		return $this->native('unlink', $path);
-	}
-
-
-	public function url_stat(string $path, int $flags)
-	{
-		try {
-			$func = $flags & STREAM_URL_STAT_LINK ? 'lstat' : 'stat';
-			return $flags & STREAM_URL_STAT_QUIET
-				? @$this->native($func, $path)
-				: $this->native($func, $path);
-		} catch (\RuntimeException $e) {
-			// SplFileInfo::isFile throws exception
-			return false;
-		}
-	}
-
-
-	private function native(string $func)
-	{
-		stream_wrapper_restore(self::PROTOCOL);
-		try {
-			return $func(...array_slice(func_get_args(), 1));
-		} finally {
-			stream_wrapper_unregister(self::PROTOCOL);
-			stream_wrapper_register(self::PROTOCOL, self::class);
-		}
+		$this->wrapper = $this->createUnderlyingWrapper();
+		return $this->wrapper->dir_opendir($path, $options);
 	}
 
 
@@ -273,5 +109,24 @@ class BypassFinals
 		}
 
 		return false;
+	}
+
+
+	/** @return object */
+	private function createUnderlyingWrapper()
+	{
+		$wrapper = new NativeWrapper;
+		$wrapper->context = $this->context;
+		return $wrapper;
+	}
+
+
+	/** @return mixed */
+	public function __call(string $method, array $args)
+	{
+		$wrapper = $this->wrapper ?? $this->createUnderlyingWrapper();
+		return method_exists($wrapper, $method)
+			? $wrapper->$method(...$args)
+			: false;
 	}
 }
---
+++ src/NativeWrapper.php
@@ -0,0 +1,212 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DG;
+
+
+/**
+ * Wrapper using native functions for working with files and directories.
+ * @internal
+ */
+class NativeWrapper
+{
+	private const PROTOCOL = 'file';
+
+	/** @var string */
+	public static $outerWrapper;
+
+	/** @var resource|null */
+	public $context;
+
+	/** @var resource|null */
+	public $handle;
+
+
+	public function dir_closedir(): void
+	{
+		closedir($this->handle);
+	}
+
+
+	public function dir_opendir(string $path, int $options): bool
+	{
+		$this->handle = $this->context
+			? $this->native('opendir', $path, $this->context)
+			: $this->native('opendir', $path);
+		return (bool) $this->handle;
+	}
+
+
+	public function dir_readdir()
+	{
+		return readdir($this->handle);
+	}
+
+
+	public function dir_rewinddir(): bool
+	{
+		rewinddir($this->handle);
+		return true;
+	}
+
+
+	public function mkdir(string $path, int $mode, int $options): bool
+	{
+		$recursive = (bool) ($options & STREAM_MKDIR_RECURSIVE);
+		return $this->context
+			? $this->native('mkdir', $path, $mode, $recursive, $this->context)
+			: $this->native('mkdir', $path, $mode, $recursive);
+	}
+
+
+	public function rename(string $pathFrom, string $pathTo): bool
+	{
+		return $this->context
+			? $this->native('rename', $pathFrom, $pathTo, $this->context)
+			: $this->native('rename', $pathFrom, $pathTo);
+	}
+
+
+	public function rmdir(string $path, int $options): bool
+	{
+		return $this->context
+			? $this->native('rmdir', $path, $this->context)
+			: $this->native('rmdir', $path);
+	}
+
+
+	public function stream_cast(int $castAs)
+	{
+		return $this->handle;
+	}
+
+
+	public function stream_close(): void
+	{
+		fclose($this->handle);
+	}
+
+
+	public function stream_eof(): bool
+	{
+		return feof($this->handle);
+	}
+
+
+	public function stream_flush(): bool
+	{
+		return fflush($this->handle);
+	}
+
+
+	public function stream_lock(int $operation): bool
+	{
+		return $operation
+			? flock($this->handle, $operation)
+			: true;
+	}
+
+
+	public function stream_metadata(string $path, int $option, $value): bool
+	{
+		switch ($option) {
+			case STREAM_META_TOUCH:
+				return $this->native('touch', $path, $value[0] ?? time(), $value[1] ?? time());
+			case STREAM_META_OWNER_NAME:
+			case STREAM_META_OWNER:
+				return $this->native('chown', $path, $value);
+			case STREAM_META_GROUP_NAME:
+			case STREAM_META_GROUP:
+				return $this->native('chgrp', $path, $value);
+			case STREAM_META_ACCESS:
+				return $this->native('chmod', $path, $value);
+		}
+
+		return false;
+	}
+
+
+	public function stream_open(string $path, string $mode, int $options, ?string &$openedPath): bool
+	{
+		$usePath = (bool) ($options & STREAM_USE_PATH);
+		$this->handle = $this->context
+			? $this->native('fopen', $path, $mode, $usePath, $this->context)
+			: $this->native('fopen', $path, $mode, $usePath);
+		return (bool) $this->handle;
+	}
+
+
+	public function stream_read(int $count)
+	{
+		return fread($this->handle, $count);
+	}
+
+
+	public function stream_seek(int $offset, int $whence = SEEK_SET): bool
+	{
+		return fseek($this->handle, $offset, $whence) === 0;
+	}
+
+
+	public function stream_set_option(int $option, int $arg1, ?int $arg2): bool
+	{
+		return false;
+	}
+
+
+	public function stream_stat()
+	{
+		return fstat($this->handle);
+	}
+
+
+	public function stream_tell()
+	{
+		return ftell($this->handle);
+	}
+
+
+	public function stream_truncate(int $newSize): bool
+	{
+		return ftruncate($this->handle, $newSize);
+	}
+
+
+	public function stream_write(string $data)
+	{
+		return fwrite($this->handle, $data);
+	}
+
+
+	public function unlink(string $path): bool
+	{
+		return $this->native('unlink', $path);
+	}
+
+
+	public function url_stat(string $path, int $flags)
+	{
+		try {
+			$func = $flags & STREAM_URL_STAT_LINK ? 'lstat' : 'stat';
+			return $flags & STREAM_URL_STAT_QUIET
+				? @$this->native($func, $path)
+				: $this->native($func, $path);
+		} catch (\RuntimeException $e) {
+			// SplFileInfo::isFile throws exception
+			return false;
+		}
+	}
+
+
+	private function native(string $func)
+	{
+		stream_wrapper_restore(self::PROTOCOL);
+		try {
+			return $func(...array_slice(func_get_args(), 1));
+		} finally {
+			stream_wrapper_unregister(self::PROTOCOL);
+			stream_wrapper_register(self::PROTOCOL, self::$outerWrapper);
+		}
+	}
+}
